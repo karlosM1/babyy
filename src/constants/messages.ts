c17f4a8911891c441f7eded783b9c5afaafaae32
Karlos Miguel Serrano <karlos_serrano2002@yahoo.com>
2026-05-15 02:36:22 +0800

    feat: update FINAL_LOVE_NOTE with personalized birthday message

--- src/constants/messages.ts
+++ src/constants/messages.ts
@@ -3,7 +3,7 @@
  */
 export const FINAL_LOVE_NOTE = `To my babyyy,
 
-Happy 23th Birthday! I made this little surprise website for you because you deserve something special. Thank you for being my favorite person, my happiness, and my safe place.
+Happy 23th Birthday! I made this little surprise for you because you deserve something special. Thank you for understanding me always, I’m grateful to have you. I can’t wait to celebrate your birthday today. And today is also your first day sa job mo. I’m proud of you kasi kahit mahirap kinaya mo. I can’t wait to see you na suot ung scrub suit mo ngayon. Excited na me sa chika mo after duty, abangan ko yan mamaya!, Dati iniisip lang natin pano kaya pag may work na tayo? pano kaya ang set up?, and we made a promise na we always make time for each other kahit busy tayo sa work para sa future. I’m proud of you sa mga achievements mo babyy. You are the most wonderful person I've ever met, you are the best part of my life. I love youuuu babyyy!!!
 
 I hope this makes you smile.
 
